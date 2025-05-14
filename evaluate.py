46728302efe8b2dda45b1b3910b51757c88b2a00
Subaru-Gastraea <davidqxzz123@gmail.com>
2025-05-14 17:45:59 +0800

    feat(predict_graphs): Generate graphs with 3 time postfixes (_early, _mid, _late)
    Skip graphs which stuck over 5 times without generating nodes with the same time postfix

--- evaluate.py
+++ evaluate.py
@@ -16,9 +16,11 @@ class ArgsEvaluate():
     def __init__(self):
         # Can manually select the device too
         self.device = torch.device(
-            'cuda:0' if torch.cuda.is_available() else 'cpu')
+            'cuda:3' if torch.cuda.is_available() else 'cpu')
 
-        self.model_path = 'model_save/' + 'DFScodeRNN_MIMIC-Lung-diff_time_2025-05-02 22:30:15/DFScodeRNN_MIMIC-Lung-diff_time_4600.dat' # 'model_name'
+        self.model_path = 'model_save/' + 'DFScodeRNN_MIMIC-Prostate-diff_time_2025-05-02 22:31:03/DFScodeRNN_MIMIC-Prostate-diff_time_4100.dat' # 'model_name'
+        # Whether the node types of graph are time-aware
+        self.diff_node_type_time = True
 
         self.num_epochs = get_model_attribute(
             'epoch', self.model_path, self.device)
@@ -26,7 +28,7 @@ class ArgsEvaluate():
         # Whether to generate networkx format graphs for real datasets
         self.generate_graphs = True
 
-        self.count = 100    # 2560   # Number of graphs to generate
+        self.count = 20000    # 2560   # Number of graphs to generate
         self.batch_size = 25    # 32  # Must be a factor of count
 
         self.metric_eval_batch_size = 256
--- graphgen/train.py
+++ graphgen/train.py
@@ -8,6 +8,13 @@ from graphgen.model import create_model
 from graphgen_utils import load_model, get_model_attribute
 from dfscode.dfs_wrapper import graph_from_dfscode
 
+import numpy as np
+from tqdm import tqdm
+import pathlib as Pathlib
+
+seed = 42
+torch.manual_seed(seed)
+np.random.seed(seed)
 
 def evaluate_loss(args, model, data, feature_map):
     x_len_unsorted = data['len'].to(args.device)
@@ -110,106 +117,284 @@ def evaluate_loss(args, model, data, feature_map):
 
 
 def predict_graphs(eval_args):
+    # 載入訓練參數與模型特徵對應資訊
     train_args = eval_args.train_args
     feature_map = get_model_attribute(
         'feature_map', eval_args.model_path, eval_args.device)
     train_args.device = eval_args.device
 
+    # Whether the node types of graph are time-aware
+    diff_node_type_time = eval_args.diff_node_type_time
+
+    # 建立模型並載入已訓練參數
     model = create_model(train_args, feature_map)
     load_model(eval_args.model_path, eval_args.device, model)
 
     for _, net in model.items():
         net.eval()
 
+    # 計算 feature 向量長度
     max_nodes = feature_map['max_nodes']
     len_node_vec, len_edge_vec = len(
         feature_map['node_forward']) + 1, len(feature_map['edge_forward']) + 1
     feature_len = 2 * (max_nodes + 1) + 2 * len_node_vec + len_edge_vec
 
     graphs = []
+    # graph_cnt = 3
 
-    for _ in range(eval_args.count // eval_args.batch_size):
-        # initialize dfs_code_rnn hidden according to batch size
-        model['dfs_code_rnn'].hidden = model['dfs_code_rnn'].init_hidden(
-            batch_size=eval_args.batch_size)
+    # mask_path = Pathlib.Path('./check_mask')
+    # if mask_path.exists():
+    #     for file in mask_path.iterdir():
+    #         if file.is_file():
+    #             file.unlink()
+    # mask_path.mkdir(parents=True, exist_ok=True)
 
-        rnn_input = torch.zeros(
-            (eval_args.batch_size, 1, feature_len), device=eval_args.device)
-        pred = torch.zeros(
-            (eval_args.batch_size, eval_args.max_num_edges, 5), device=eval_args.device)
+    if diff_node_type_time:
+        postfixes = ['_early', '_mid', '_late']
+        nb = feature_map['node_backward']
+        postfix_mask = [np.array([False] * len(nb) + [True]) for _ in range(len(postfixes))]
+        for i, node in nb.items():
+            for j, postfix in enumerate(postfixes):
+                if node.endswith(postfix):
+                    postfix_mask[j][i] = True
 
-        for i in range(eval_args.max_num_edges):
-            rnn_output = model['dfs_code_rnn'](rnn_input)
+                # mask_file = mask_path / f'mask{postfix}.csv'
+                # with open(mask_file, 'a') as f:
+                #     f.write(f'{i},{node},{postfix_mask[j][i]}\n')
 
-            # Evaluating dfscode tuple
-            timestamp1 = model['output_timestamp1'](
-                rnn_output).reshape(eval_args.batch_size, -1)
-            timestamp2 = model['output_timestamp2'](
-                rnn_output).reshape(eval_args.batch_size, -1)
-            vertex1 = model['output_vertex1'](
-                rnn_output).reshape(eval_args.batch_size, -1)
-            edge = model['output_edge'](rnn_output).reshape(
-                eval_args.batch_size, -1)
-            vertex2 = model['output_vertex2'](
-                rnn_output).reshape(eval_args.batch_size, -1)
+    def pred_step(model, rnn_input):
+        # rnn_output (h_i) = LSTM(h_{i-1}, Embed(s_{i-1}))
+        rnn_output = model['dfs_code_rnn'](rnn_input)
 
-            if train_args.loss_type == 'BCE':
-                timestamp1 = Categorical(timestamp1).sample()
-                timestamp2 = Categorical(timestamp2).sample()
-                vertex1 = Categorical(vertex1).sample()
-                edge = Categorical(edge).sample()
-                vertex2 = Categorical(vertex2).sample()
+        # Evaluating dfscode tuple
+        # 模型輸出五個分支：timestamp1, timestamp2, vertex1, edge, vertex2
+        # MLP layer (Linear + ReLU + Softmax)
+        timestamp1 = model['output_timestamp1'](
+            rnn_output).reshape(eval_args.batch_size, -1)
+        timestamp2 = model['output_timestamp2'](
+            rnn_output).reshape(eval_args.batch_size, -1)
+        vertex1 = model['output_vertex1'](
+            rnn_output).reshape(eval_args.batch_size, -1)
+        edge = model['output_edge'](rnn_output).reshape(
+            eval_args.batch_size, -1)
+        vertex2 = model['output_vertex2'](
+            rnn_output).reshape(eval_args.batch_size, -1)
 
-            elif train_args.loss_type == 'NLL':
-                timestamp1 = Categorical(logits=timestamp1).sample()
-                timestamp2 = Categorical(logits=timestamp2).sample()
-                vertex1 = Categorical(logits=vertex1).sample()
-                edge = Categorical(logits=edge).sample()
-                vertex2 = Categorical(logits=vertex2).sample()
+        # 根據損失函數類型進行 sampling
+        if train_args.loss_type == 'BCE':
+            timestamp1 = Categorical(timestamp1).sample()
+            timestamp2 = Categorical(timestamp2).sample()
+            vertex1 = Categorical(vertex1).sample()
+            edge = Categorical(edge).sample()
+            vertex2 = Categorical(vertex2).sample()
 
+        elif train_args.loss_type == 'NLL':
+            timestamp1 = Categorical(logits=timestamp1).sample()
+            timestamp2 = Categorical(logits=timestamp2).sample()
+            vertex1 = Categorical(logits=vertex1).sample()
+            edge = Categorical(logits=edge).sample()
+            vertex2 = Categorical(logits=vertex2).sample()
+
+        return timestamp1, timestamp2, vertex1, edge, vertex2
+
+    # check_path = Pathlib.Path('./check_batch_fix')
+    # if check_path.exists():
+    #     for file in check_path.iterdir():
+    #         if file.is_file():
+    #             file.unlink()
+    # check_path.mkdir(parents=True, exist_ok=True)
+
+    # 每個 batch 產生一批圖
+    with torch.no_grad():
+        for _ in range(eval_args.count // eval_args.batch_size):
+            # initialize dfs_code_rnn hidden according to batch size
+            model['dfs_code_rnn'].hidden = model['dfs_code_rnn'].init_hidden(
+                batch_size=eval_args.batch_size)
+
+            # 初始化 RNN 輸入與輸出空間
             rnn_input = torch.zeros(
                 (eval_args.batch_size, 1, feature_len), device=eval_args.device)
+            pred = torch.zeros(
+                (eval_args.batch_size, eval_args.max_num_edges, 5), device=eval_args.device)
 
-            rnn_input[torch.arange(eval_args.batch_size), 0, timestamp1] = 1
-            rnn_input[torch.arange(eval_args.batch_size),
-                      0, max_nodes + 1 + timestamp2] = 1
-            rnn_input[torch.arange(eval_args.batch_size),
-                      0, 2 * max_nodes + 2 + vertex1] = 1
-            rnn_input[torch.arange(eval_args.batch_size), 0,
-                      2 * max_nodes + 2 + len_node_vec + edge] = 1
-            rnn_input[torch.arange(eval_args.batch_size), 0, 2 *
-                      max_nodes + 2 + len_node_vec + len_edge_vec + vertex2] = 1
+            if diff_node_type_time:
+                batch_sample_mask = np.array([False] * eval_args.batch_size)
+                batch_sample_repeat_cnt = [0] * eval_args.batch_size
+                tmp_rnn_input = torch.zeros(
+                    (eval_args.batch_size, 1, feature_len), device=eval_args.device)
+                
+                batch_postfix_mask_i = [-1] * eval_args.batch_size
 
-            pred[:, i, 0] = timestamp1
-            pred[:, i, 1] = timestamp2
-            pred[:, i, 2] = vertex1
-            pred[:, i, 3] = edge
-            pred[:, i, 4] = vertex2
+            # 每個 batch 逐步產生 DFS code 中的 edge tuples
+            for step_i in tqdm(range(eval_args.max_num_edges), total=eval_args.max_num_edges, desc='Generating graphs (steps)'):
+                
+                if diff_node_type_time:
+                    repeat_flag = True
+                    for batch_sample_i in range(eval_args.batch_size):
+                        if batch_sample_repeat_cnt[batch_sample_i] < 5:
+                            batch_sample_repeat_cnt[batch_sample_i] = 0     # Reset repeat count each step
 
-        nb = feature_map['node_backward']
-        eb = feature_map['edge_backward']
-        for i in range(eval_args.batch_size):
-            dfscode = []
-            for j in range(eval_args.max_num_edges):
-                if pred[i, j, 0] == max_nodes or pred[i, j, 1] == max_nodes \
-                        or pred[i, j, 2] == len_node_vec - 1 or pred[i, j, 3] == len_edge_vec - 1 \
-                        or pred[i, j, 4] == len_node_vec - 1:
-                    break
+                    while repeat_flag:
+                        timestamp1, timestamp2, vertex1, edge, vertex2 = pred_step(model, rnn_input)
 
-                dfscode.append(
-                    (int(pred[i, j, 0].data), int(pred[i, j, 1].data), nb[int(pred[i, j, 2].data)],
-                     eb[int(pred[i, j, 3].data)], nb[int(pred[i, j, 4].data)]))
+                        # Filter sampled vertices to include only nodes with the desired postfix
+                        for batch_sample_i in range(eval_args.batch_size):
 
-            graph = graph_from_dfscode(dfscode)
+                            if batch_sample_repeat_cnt[batch_sample_i] >= 5:    # Skip this sample (Not used for graph generation)
+                                batch_sample_mask[batch_sample_i] = True
 
-            # Remove self loops
-            graph.remove_edges_from(nx.selfloop_edges(graph))
+                            if batch_sample_mask[batch_sample_i]:
+                                continue
 
-            # Take maximum connected component
-            if len(graph.nodes()):
-                max_comp = max(nx.connected_components(graph), key=len)
-                graph = nx.Graph(graph.subgraph(max_comp))
+                            # check_file = check_path / f'check_{batch_sample_i}.csv'
+                            # nb = feature_map['node_backward']
+                            # with open(check_file, 'a') as f:
+                            #     v1 = nb.get(int(vertex1[batch_sample_i].data), 'End')
+                            #     v2 = nb.get(int(vertex2[batch_sample_i].data), 'End')
+                            #     f.write(f'{step_i},{v1},{v2},{int(vertex1[batch_sample_i].data)},{int(vertex2[batch_sample_i].data)}\n')
 
-            graphs.append(graph)
+                            if batch_postfix_mask_i[batch_sample_i] == -1:
+                                for postfix_i, mask in enumerate(postfix_mask):
+                                    if mask[vertex1[batch_sample_i]] == True \
+                                        and mask[vertex2[batch_sample_i]] == True:
+                                        # If the sampled vertex1 and vertex2 has the desired postfix, set the mask
+                                        batch_postfix_mask_i[batch_sample_i] = postfix_i
+                                        batch_sample_mask[batch_sample_i] = True
+
+                                        # 組合為下一步的暫存 RNN input（One-hot）
+                                        tmp_rnn_input[batch_sample_i, 0, timestamp1[batch_sample_i]] = 1
+                                        tmp_rnn_input[batch_sample_i,
+                                                0, max_nodes + 1 + timestamp2[batch_sample_i]] = 1
+                                        tmp_rnn_input[batch_sample_i,
+                                                0, 2 * max_nodes + 2 + vertex1[batch_sample_i]] = 1
+                                        tmp_rnn_input[batch_sample_i, 0,
+                                                2 * max_nodes + 2 + len_node_vec + edge[batch_sample_i]] = 1
+                                        tmp_rnn_input[batch_sample_i, 0, 2 *
+                                                max_nodes + 2 + len_node_vec + len_edge_vec + vertex2[batch_sample_i]] = 1
+
+                                        # 儲存 sample 結果
+                                        pred[batch_sample_i, step_i, 0] = timestamp1[batch_sample_i]
+                                        pred[batch_sample_i, step_i, 1] = timestamp2[batch_sample_i]
+                                        pred[batch_sample_i, step_i, 2] = vertex1[batch_sample_i]
+                                        pred[batch_sample_i, step_i, 3] = edge[batch_sample_i]
+                                        pred[batch_sample_i, step_i, 4] = vertex2[batch_sample_i]
+                                        
+                                        break
+
+                            else:
+                                target_postfix_mask = postfix_mask[batch_postfix_mask_i[batch_sample_i]]
+
+                                if target_postfix_mask[vertex1[batch_sample_i]] == True \
+                                    and target_postfix_mask[vertex2[batch_sample_i]] == True:
+                                    # If the sampled vertex1 and vertex2 has the desired postfix, set the mask
+                                    batch_sample_mask[batch_sample_i] = True
+
+                                    # 組合為下一步的暫存 RNN input（One-hot）
+                                    tmp_rnn_input[batch_sample_i, 0, timestamp1[batch_sample_i]] = 1
+                                    tmp_rnn_input[batch_sample_i,
+                                            0, max_nodes + 1 + timestamp2[batch_sample_i]] = 1
+                                    tmp_rnn_input[batch_sample_i,
+                                            0, 2 * max_nodes + 2 + vertex1[batch_sample_i]] = 1
+                                    tmp_rnn_input[batch_sample_i, 0,
+                                            2 * max_nodes + 2 + len_node_vec + edge[batch_sample_i]] = 1
+                                    tmp_rnn_input[batch_sample_i, 0, 2 *
+                                            max_nodes + 2 + len_node_vec + len_edge_vec + vertex2[batch_sample_i]] = 1
+
+                                    # 儲存 sample 結果
+                                    pred[batch_sample_i, step_i, 0] = timestamp1[batch_sample_i]
+                                    pred[batch_sample_i, step_i, 1] = timestamp2[batch_sample_i]
+                                    pred[batch_sample_i, step_i, 2] = vertex1[batch_sample_i]
+                                    pred[batch_sample_i, step_i, 3] = edge[batch_sample_i]
+                                    pred[batch_sample_i, step_i, 4] = vertex2[batch_sample_i]
+
+                        # if batch_sample_mask.sum() >= eval_args.batch_size // 3:
+                        if batch_sample_mask.all():
+
+                            # 組合為下一步的 RNN input（One-hot）
+                            rnn_input = tmp_rnn_input.clone().detach()
+                            batch_sample_mask = np.array([False] * eval_args.batch_size)
+                            tmp_rnn_input = torch.zeros(
+                                (eval_args.batch_size, 1, feature_len), device=eval_args.device)
+
+                            repeat_flag = False
+
+                        # print('batch_sample_mask:\n', batch_sample_mask)
+                        # print()
+
+                        else:
+                            for idx in np.where(~batch_sample_mask)[0]:
+                                batch_sample_repeat_cnt[idx] += 1
+
+                else:
+                    timestamp1, timestamp2, vertex1, edge, vertex2 = pred_step(model, rnn_input)
+
+                    # 組合為下一步的 RNN input（One-hot）
+                    rnn_input = torch.zeros(
+                        (eval_args.batch_size, 1, feature_len), device=eval_args.device)
+
+                    rnn_input[torch.arange(eval_args.batch_size), 0, timestamp1] = 1
+                    rnn_input[torch.arange(eval_args.batch_size),
+                            0, max_nodes + 1 + timestamp2] = 1
+                    rnn_input[torch.arange(eval_args.batch_size),
+                            0, 2 * max_nodes + 2 + vertex1] = 1
+                    rnn_input[torch.arange(eval_args.batch_size), 0,
+                            2 * max_nodes + 2 + len_node_vec + edge] = 1
+                    rnn_input[torch.arange(eval_args.batch_size), 0, 2 *
+                            max_nodes + 2 + len_node_vec + len_edge_vec + vertex2] = 1
+
+                    # 儲存 sample 結果
+                    pred[:, step_i, 0] = timestamp1
+                    pred[:, step_i, 1] = timestamp2
+                    pred[:, step_i, 2] = vertex1
+                    pred[:, step_i, 3] = edge
+                    pred[:, step_i, 4] = vertex2
+
+                    # for batch_sample_i in range(eval_args.batch_size):
+                    #     check_file = check_path / f'check_{batch_sample_i}.csv'
+                    #     nb = feature_map['node_backward']
+                    #     with open(check_file, 'a') as f:
+                    #         v1 = nb.get(int(vertex1[batch_sample_i].data), 'End')
+                    #         v2 = nb.get(int(vertex2[batch_sample_i].data), 'End')
+                    #         f.write(f'{step_i},{v1},{v2},{int(vertex1[batch_sample_i].data)},{int(vertex2[batch_sample_i].data)}\n')
+
+            # 將 DFS code 轉換為 NetworkX graph
+            nb = feature_map['node_backward']
+            eb = feature_map['edge_backward']
+            for i in range(eval_args.batch_size):
+                if batch_sample_repeat_cnt[i] >= 5:   # Skip this sample (Not used for graph generation)
+                    continue
+
+                dfscode = []
+                for j in range(eval_args.max_num_edges):
+                    if pred[i, j, 0] == max_nodes or pred[i, j, 1] == max_nodes \
+                            or pred[i, j, 2] == len_node_vec - 1 or pred[i, j, 3] == len_edge_vec - 1 \
+                            or pred[i, j, 4] == len_node_vec - 1:
+                        break
+
+                    dfscode.append(
+                        (int(pred[i, j, 0].data), int(pred[i, j, 1].data), nb[int(pred[i, j, 2].data)],
+                        eb[int(pred[i, j, 3].data)], nb[int(pred[i, j, 4].data)]))
+
+                graph = graph_from_dfscode(dfscode)
+
+                # Remove self loops
+                graph.remove_edges_from(nx.selfloop_edges(graph))
+
+                # Take maximum connected component
+                if len(graph.nodes()):
+                    max_comp = max(nx.connected_components(graph), key=len)
+                    graph = nx.Graph(graph.subgraph(max_comp))
+
+                # if len(graph.nodes()) > 0:
+                # print('dfscode:', dfscode)  ######
+                # print('original graph:')
+                # print(graph.nodes(data=True))
+                # print(graph.edges(data=True))
+                # print('\n\n')
+                # graph_cnt -= 1
+                # if graph_cnt == 0:
+                #     exit()
+
+                graphs.append(graph)
 
     return graphs
